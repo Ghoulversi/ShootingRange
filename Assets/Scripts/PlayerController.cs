9a75edd84b065106a20ff1baf9a6d7cd5cbc4a26
Valeriu Rusu <valeriu.r@iron-will.et>
2021-05-12 00:52:48 +0300

    Add shooting mechanic, gameplay scene and target for test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Weapon Rifle;
+    public Camera Camera;
+    public FixedJoystick FixedJoystick;
+    public Transform SpawnWeaponPoint;
+    public float MovementSensitivity = 100f;
+    public float MaxDistance;
+
+    private float _xRotation = 0f;
+
+    private void Start()
+    {
+        Instantiate(Rifle.WeaponPrefab, SpawnWeaponPoint.position, SpawnWeaponPoint.rotation, Camera.transform);
+    }
+
+
+    private void Update()
+    {
+        float joystickX = FixedJoystick.Direction.x * MovementSensitivity * Time.deltaTime;
+        float joystickY = FixedJoystick.Direction.y * MovementSensitivity * Time.deltaTime;
+
+        _xRotation -= joystickY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        Camera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * joystickX);
+    }
+
+    //Check with Raycast if hit something by pressing shoot
+    public void Shoot()
+    {
+
+        if (Physics.Raycast(Camera.transform.position, Camera.transform.forward, out var hit, MaxDistance))
+        {
+            Debug.Log($"Hit: {hit.transform.name}");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Transform[] PositionsToMove;
+    public GameObject TargetModel;
+
+    [Header("Settings")] 
+    public float MovementSpeed;
+
+    private int _nextPosIndex;
+    private Transform _nextPos;
+
+    private void Start()
+    {
+        _nextPos = PositionsToMove[0];
+    }
+
+    private void Update()
+    {
+        MoveTarget();
+    }
+
+    private void MoveTarget()
+    {
+        if (TargetModel.transform.position == _nextPos.position)
+        {
+            _nextPosIndex++;
+            if (_nextPosIndex >= PositionsToMove.Length)
+            {
+                _nextPosIndex = 0;
+            }
+
+            _nextPos = PositionsToMove[_nextPosIndex];
+        }
+        else
+        {
+            TargetModel.transform.position = Vector3.MoveTowards(TargetModel.transform.position, _nextPos.position, MovementSpeed * Time.deltaTime);
+        }
+    }
+}
